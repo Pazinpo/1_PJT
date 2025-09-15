5783f5a3b21ceb12af01ab7cf5d6da59e866859f
김민재 <akswo184@gmail.com>
2025-09-15 09:34:16 +0900

    로그인 페이지 초기ver

--- health-routine-tracker-frontend/src/App.js
+++ health-routine-tracker-frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./page/LoginPage"; // 네가 만든 로그인 페이지 경로
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/blog" element={<div>블로그 페이지 (임시)</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
